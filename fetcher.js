6eeb3f324eca70742618251d9ba1242557f598ed
Katie Liu <katieliu59@gmail.com>
2023-01-18 21:38:53 -0500

    Integrate file stats to save content requested in the new file, implement logic to retrieve the file size in byte and console log a message accordingly

--- fetcher.js
+++ fetcher.js
@@ -1,28 +1,25 @@
-const request = require('request');
-
+const request = require("request");
+const fs = require("fs");
 
 let args = [];
 for (let arg = 0; arg < process.argv.length - 2; arg++) {
 	args.push(process.argv[arg + 2]);
-};
-
-
-request(`${args[0]}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-});
-
+}
 // console.log(args); -> logs args into array [ 'http://www.example.edu/', './index.html' ]
 
-const fs = require('fs');
+request(`${args[0]}`, (error, response, body) => {
+	"error:", error; // Print the error if one occurred
+	"statusCode:", response && response.statusCode; // Print the response status code if a response was received
+	"body:", body; // Print the HTML for the link arg
 
-const content = 'Some content!'; //change 
+	const content = body;
+    const fileSize = content.length;
 
-fs.writeFile(`${args[1]}`, content, err => {
-  if (err) {
-    console.error(err);
-  }
-  // file written successfully
-  
+    fs.writeFile(`${args[1]}`, content, (err) => {
+        if (err) {
+            console.error(err);
+		}
+		// file written successfully
+    });
+    console.log(`Downloaded and save ${fileSize} to ${args[1]}.`)
 });
